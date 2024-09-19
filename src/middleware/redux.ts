e13012975f1283c642d5ff7118c9544a4828a08f
Forest <767425412lin@gmail.com>
2024-09-19 17:19:19 +0800

    feat: 添加注释

--- src/middleware/redux.ts
+++ src/middleware/redux.ts
@@ -3,11 +3,12 @@ import type { NamedSet } from './devtools.ts'
 
 type Write<T, U> = Omit<T, keyof U> & U
 
+// 定义一个 Redux 动作对象，包含一个 type 属性
 type Action = { type: string }
 
 type StoreRedux<A> = {
   dispatch: (a: A) => A
-  dispatchFromDevtools: true
+  dispatchFromDevtools: true // 是否支持从 Redux DevTools 分发 action
 }
 
 type ReduxState<A> = {
@@ -32,18 +33,20 @@ declare module '../vanilla' {
 }
 
 type ReduxImpl = <T, A extends Action>(
-  reducer: (state: T, action: A) => T,
-  initialState: T,
+  reducer: (state: T, action: A) => T, // 根据 action 更新状态
+  initialState: T, // 初始状态
 ) => StateCreator<T & ReduxState<A>, [], []>
 
 const reduxImpl: ReduxImpl = (reducer, initial) => (set, _get, api) => {
   type S = typeof initial
   type A = Parameters<typeof reducer>[1]
-  ;(api as any).dispatch = (action: A) => {
-    ;(set as NamedSet<S>)((state: S) => reducer(state, action), false, action)
-    return action
-  }
-  ;(api as any).dispatchFromDevtools = true
+    // 定义 dispatch 方法
+    ; (api as any).dispatch = (action: A) => {
+      ; (set as NamedSet<S>)((state: S) => reducer(state, action), false, action)
+      return action
+    }
+    // 支持 Redux DevTools 的调试
+    ; (api as any).dispatchFromDevtools = true
 
   return { dispatch: (...a) => (api as any).dispatch(...a), ...initial }
 }
--- src/middleware/subscribeWithSelector.ts
+++ src/middleware/subscribeWithSelector.ts
@@ -43,29 +43,37 @@ type SubscribeWithSelectorImpl = <T extends object>(
   storeInitializer: StateCreator<T, [], []>,
 ) => StateCreator<T, [], []>
 
+// 接收一个 store 初始化函数 fn，并返回一个新的 store 创建函数
 const subscribeWithSelectorImpl: SubscribeWithSelectorImpl =
   (fn) => (set, get, api) => {
     type S = ReturnType<typeof fn>
     type Listener = (state: S, previousState: S) => void
+    // 暂存原来的 subscribe
     const origSubscribe = api.subscribe as (listener: Listener) => () => void
+    // 在原来 subscribe 的基础上扩展
     api.subscribe = ((selector: any, optListener: any, options: any) => {
-      let listener: Listener = selector // if no selector
+      let listener: Listener = selector // 如果没有选择器，直接使用传入的监听器
       if (optListener) {
+        // 比较选择器返回的新旧值，默认是 Object.is 方法
         const equalityFn = options?.equalityFn || Object.is
         let currentSlice = selector(api.getState())
         listener = (state) => {
           const nextSlice = selector(state)
+          // 值有变化，则调用监听器
           if (!equalityFn(currentSlice, nextSlice)) {
             const previousSlice = currentSlice
             optListener((currentSlice = nextSlice), previousSlice)
           }
         }
+        // 如果设置了 fireImmediately 选项，立即触发一次监听器
         if (options?.fireImmediately) {
           optListener(currentSlice, currentSlice)
         }
       }
+      // 调用原始的 subscribe 方法来注册这个新创建的监听器并返回
       return origSubscribe(listener)
     }) as any
+    // 调用原始的 store 初始化函数 fn，并返回初始状态
     const initialState = fn(set, get, api)
     return initialState
   }
